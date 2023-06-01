8dacb5ba9e5c42ca431736c9c661650aa203359f
Alvin Le <alvinlester24@gmail.com>
2023-05-31 20:32:45 -0700

    started movie database

---
+++ src/MovieDatabase.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+
+/**
+ * This class represents the storage of a list of movies.
+ *
+ * @author Alvin Le
+ */
+public class MovieDatabase {
+    private static Map<String, Movie> database;
+    public MovieDatabase(String fileName) {
+
+    }
+}
