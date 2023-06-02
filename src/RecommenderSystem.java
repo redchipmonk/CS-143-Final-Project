8937b0507821c5b0c1a9cefd32e34def406855de
Alvin Le <alvinlester24@gmail.com>
2023-06-01 23:09:56 -0700

    finished UserNode.java

--- src/RecommenderSystem.java
+++ src/RecommenderSystem.java
@@ -1,3 +1,20 @@
-public class RecommenderSystem {
+import java.util.ArrayList;
+import java.util.List;
 
+public class RecommenderSystem {
+    private List<Movie> movies;
+    public RecommenderSystem() {
+        movies = new ArrayList<>();
+    }
+    public void sortByTitle() {
+
+    }
+
+    public void sortByRating() {
+
+    }
+
+    public void sortByGenre() {
+
+    }
 }
